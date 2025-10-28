c0a2593f6a80104036f81e641d37d271ba2ce83c
ravibs298 <ravibs2908@gmail.com>
2025-10-28 15:38:31 +0530

    react

---
+++ Day 11/greet.js
@@ -0,0 +1,17 @@
+const reactElement = <>
+    <div>
+        <div>
+            <img alt="img-here" src="" />
+            <h4>Heading</h4>
+        </div>
+    </div>
+</>
+
+const ReactComponent = () => {
+    return (<>ReactComponent </>);
+}
+
+const Greet2 = () => {
+    return (<ReactComponent />);
+}
+export default Greet2;
